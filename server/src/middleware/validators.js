a7bcd4176ad2601f59bd23f017cc8531af866519
harsh-1214 <harshkhasgiwale@gmail.com>
2026-04-09 21:03:12 +0530

    Changing password length

--- server/src/middleware/validators.js
+++ server/src/middleware/validators.js
@@ -8,7 +8,7 @@ export const createUserValidator = [
   body("name").trim().notEmpty().withMessage("Name is required"),
   body("email").isEmail().withMessage("Valid email is required"),
   body("password")
-    .isLength({ min: 8 })
+    .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters"),
   body("role")
     .isIn(roles)
